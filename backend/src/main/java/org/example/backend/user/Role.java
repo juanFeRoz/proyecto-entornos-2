1ab9a846ec9e70d8f7b0e8587d20f411e9f5399c
Juan Felipe Rojas <juanfeliperojas2@protonmail.com>
2025-05-21 10:57:49 -0500

    Implementacion api de registro y login

---
+++ backend/src/main/java/org/example/backend/user/Role.java
@@ -0,0 +1,17 @@
+package org.example.backend.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+}
