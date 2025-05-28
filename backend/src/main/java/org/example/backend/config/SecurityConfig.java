ca7609c37580b93d5bf8618ea74c37cae9a7faf0
Juan Felipe Rojas <juanfeliperojas2@protonmail.com>
2025-05-27 19:22:03 -0500

    arregle el puerto de conexion con el front

--- backend/src/main/java/org/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Angular
+        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Angular
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // ✅ importante para que funcione JSESSIONID
